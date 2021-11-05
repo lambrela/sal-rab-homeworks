f0847b8fd6d10ce0016b91b7db8ea7a3e5757666
Venera Buriak <lambre_la@bk.ru>
2021-11-05 18:03:31 +0700

    Выполнение задания №3

--- task_3/solution.js
+++ task_3/solution.js
@@ -25,5 +25,15 @@ function renderCartItem(item) {
 
     // Конец решения задания №3.1.
 
+    let itemCountText = ``;
+    itemCountText += product.count;
+    itemCountText += ` × `;
+    itemCountText += product.price;
+    itemCountText += ` ₽ = `;
+    let sum = product.count*product.price;
+    itemCountText += sum;
+    itemCountText += ` ₽`;
+
+
     return `<div class="cart-item"><div>${product.title}</div><div>${itemCountText}</div></div>`;
 }
